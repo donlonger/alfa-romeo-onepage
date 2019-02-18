2d96cdc014a9b12c006cf15b5e370f8dce4e6763
donlonger <lachowski_m@yahoo.com>
2019-02-18 02:28:12 +0100

    stworzenie sekcji top, headline i galerii w wersji tylko z owl carouselem bez responsywności

---
+++ build/scripts/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".owl-carousel").owlCarousel(),AOS.init(),$("#top #burger").click(function(){$(this).toggleClass("open"),$(this).next(".menu").fadeToggle(),$(this).prev(".logo").toggleClass("open")}),$("#top .menu a").click(function(){$("#top #burger").removeClass("open"),$("#top .menu").fadeOut(),$("#top .logo").removeClass("open")})});
