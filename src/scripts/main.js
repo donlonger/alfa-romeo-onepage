cb2ce64d5ec5ed867262cc08818918a1224905f4
donlonger <lachowski_m@yahoo.com>
2019-02-20 01:03:31 +0100

    dodanie menu fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
+(function(fn) {
+    'use strict';
+	fn.scrollTop = $(this).scrollTop();
+
+	fn.updateVars = function() {
+		fn.window = {
+			x: $(window).width(),
+			y: $(window).height()
+		};
+		fn.scrollTop = $(window).scrollTop();
+	};
+
+	fn.getWindow = function() {
+		return fn.window;
+	};
+
+	fn.getScrollTop = function() {
+		return fn.scrollTop;
+	};
+}(window.fn = window.fn || {}));  
+
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
             navigation : true  
@@ -5,15 +26,26 @@ $(document).ready(function(){
 
     AOS.init();
 
-    $("#top #burger").click(function () {
+    $(".nav-bar #burger").click(function () {
 		$(this).toggleClass("open");
         $(this).next(".menu").fadeToggle();
         $(this).prev(".logo").toggleClass("open");
     });
 
-	$("#top .menu a").click(function () {
-        $("#top #burger").removeClass("open");
-        $("#top .menu").fadeOut();
-        $("#top .logo").removeClass("open");
+	$(".nav-bar  .menu a").click(function () {
+        $(".nav-bar  #burger").removeClass("open");
+        $(".nav-bar  .menu").fadeOut();
+        $(".nav-bar  .logo").removeClass("open");
     });
+
+    $(window).scroll(function() {
+        fn.updateVars();
+        console.log(fn);
+        if (fn.scrollTop > 110) {
+            $(".top-wrapper").addClass("fixed");        
+        } else if (fn.scrollTop <= 110) {
+            $(".top-wrapper").removeClass("fixed");    
+        }
+    });
+
   });
